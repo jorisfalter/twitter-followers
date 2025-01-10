682c3c5454b760df545f71278361db16bbc0cf10
Joris Falter <joris@jorisfalter.com>
2025-01-10 13:24:39 +0100

    x

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def fetch_followers_list(twitter_handle, preview_run, limitOn):
         maxFollowers = 200;
         print("limit is on")
     elif limitOn is False:
-        maxFollowers = 1000; # change this when in production!
+        maxFollowers = 8000; # change this when in production!
         print("limit is off")
 
 
